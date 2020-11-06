63f334378fd22ed335d75aef4436df4ec3bd9c61
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-06 23:57:53 +0100

    Update CocheController.java

--- src/main/java/com/example/definitivo/controllers/CocheController.java
+++ src/main/java/com/example/definitivo/controllers/CocheController.java
@@ -1,4 +1,16 @@
 package com.example.definitivo.controllers;
 
+import com.example.definitivo.entities.Coche;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CocheController {
+    public List<Coche> getCoches();
+    public Optional<Coche> getCocheById(Integer id);
+    public Coche addCoche(Coche cocheNew);
+    public String deleteCoche(Integer id);
+    public String updateCoche(Coche cocheUpdated);
+
+    public String test();
 }
