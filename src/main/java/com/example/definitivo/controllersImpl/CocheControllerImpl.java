53c2508d46cd624588221047b9c38a6d94e2a3ff
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-07 00:17:40 +0100

    Update CocheControllerImpl.java

--- src/main/java/com/example/definitivo/controllersImpl/CocheControllerImpl.java
+++ src/main/java/com/example/definitivo/controllersImpl/CocheControllerImpl.java
@@ -2,19 +2,27 @@ package com.example.definitivo.controllersImpl;
 
 import com.example.definitivo.controllers.CocheController;
 import com.example.definitivo.entities.Coche;
+import com.example.definitivo.repositories.CocheRespository;
 import com.example.definitivo.services.CocheService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
 public class CocheControllerImpl implements CocheController {
+
     @Autowired
     CocheService cocheService;
 
+    // http://localhost:8080/coches (GET)
+    @RequestMapping(value = "/coches", method = RequestMethod.GET, produces = "application/json")
     @Override
     public List<Coche> getCoches() {
-        return null;
+        return cocheService.findAllCoches();
     }
 
     @Override
