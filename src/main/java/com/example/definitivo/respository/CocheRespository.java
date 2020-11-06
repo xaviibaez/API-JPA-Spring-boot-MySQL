bdc87e7bef974a0a8b5db7b1f8d7bdc90225da11
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-06 23:52:38 +0100

    delete unnecesary

--- src/main/java/com/example/definitivo/respository/CocheRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.definitivo.respository;
-
-import com.example.definitivo.entity.Coche;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CocheRespository  extends JpaRepository<Coche, Integer> {
-}
