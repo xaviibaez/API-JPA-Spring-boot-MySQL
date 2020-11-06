c1f72cc35f0ebc4fbdd9b5bd49266aa5017a0fb1
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-06 23:51:50 +0100

    Create CocheService.java

---
+++ src/main/java/com/example/definitivo/services/CocheService.java
@@ -0,0 +1,14 @@
+package com.example.definitivo.services;
+
+import com.example.definitivo.entities.Coche;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CocheService {
+    public List<Coche> findAllCoches();
+    public Optional<Coche> findCocheById(Integer id);
+    public Coche saveCoche(Coche cocheNew);
+    public String deleteCoche(Integer id);
+    public String updateCoche(Coche cocheUpdated);
+}
