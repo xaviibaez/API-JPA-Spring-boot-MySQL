6990222c810996b942fecca9ffbc24e9864ae36b
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-11-07 00:17:31 +0100

    Update CocheRespository.java

--- src/main/java/com/example/definitivo/repositories/CocheRespository.java
+++ src/main/java/com/example/definitivo/repositories/CocheRespository.java
@@ -4,6 +4,9 @@ import com.example.definitivo.entities.Coche;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CocheRespository  extends JpaRepository<Coche, Integer> {
+    Void save(Optional<Coche> cocheToUpdate);
 }
